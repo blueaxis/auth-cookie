a31f4762cce28d103448d45ac39a36ebb5287398
Alarcon Ace Belen <alarconacebelen@gmail.com>
2022-03-10 21:32:25 +0800

    Add precision-recall curve plot

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 # Train and optimize random forest hyperparameters
+import pickle
+from os.path import isfile
+import matplotlib.pyplot as plt
+
 import pandas as pd
 import keras_tuner as kt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn import metrics
-import pickle
 
 # Read training features
 ds = pd.read_csv("data/features.csv")
@@ -24,52 +27,76 @@ y = normalized_ds[["Class"]]
 X = normalized_ds.drop("Class", axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+
 def build_model(hp):
     model = RandomForestClassifier(
-        n_estimators= hp.Int("n_estimators", 100, 300, step= 10),
-        criterion= hp.Choice("criterion", ["gini", "entropy"]),
-        max_depth= hp.Int("max_depth", 6, 9, step= 1),
-        min_samples_split= hp.Int("min_samples_split", 20, 40, step= 1),
-        max_features= hp.Choice("max_features", ["auto", "log2"]),
-        class_weight= {0: (2546 / (2204 * 2.0)), 1: (2546 / (342 * 2.0))},
-        max_leaf_nodes= hp.Int("max_leaf_nodes", 70, 160, step= 5),
-        oob_score= "True",
+        n_estimators=hp.Int("n_estimators", 100, 300, step=10),
+        criterion=hp.Choice("criterion", ["gini", "entropy"]),
+        max_depth=hp.Int("max_depth", 6, 9, step=1),
+        min_samples_split=hp.Int("min_samples_split", 20, 40, step=1),
+        max_features=hp.Choice("max_features", ["auto", "log2"]),
+        class_weight={0: (2546 / (2204 * 2.0)), 1: (2546 / (342 * 2.0))},
+        max_leaf_nodes=hp.Int("max_leaf_nodes", 70, 160, step=5),
+        oob_score="True",
     )
     return model
 
-# Initialize the Bayesian Search Tuner that maximizes the F2-score
-tuner = kt.tuners.SklearnTuner(
-    oracle=kt.oracles.BayesianOptimizationOracle(
-        objective=kt.Objective("score", "max"),
-        max_trials=100
-		),
-    hypermodel=build_model,
-    scoring=metrics.make_scorer(metrics.fbeta_score, beta=2),
-    cv=StratifiedKFold(10),
-    directory='./model/',
-    project_name='random_forest'
+
+def evaluate_model(model, x_, y_true):
+    y_pred = model.predict(x_)
+    tn, fp, fn, tp = metrics.confusion_matrix(y_true, y_pred).ravel()
+    print("Specificity:", tn / (tn + fp))
+    print("Sensitivity/Recall:", metrics.recall_score(y_true, y_pred))
+    print("Precision:", metrics.precision_score(y_true, y_pred))
+    print("F2-Score:", metrics.fbeta_score(y_true, y_pred, beta=2))
+    print("Confusion Matrix:")
+    print("tp:", tp, "fn:", fn)
+    print("fp:", fp, "tn:", tn)
+    print("PR-AUC:", metrics.average_precision_score(y_true, y_pred))
+
+    precision, recall, thresholds = metrics.precision_recall_curve(y_true, y_pred)
+    fig, ax = plt.subplots()
+    ax.plot(recall, precision)
+    ax.set_title('Precision-Recall Curve')
+    ax.set_ylabel('Precision')
+    ax.set_xlabel('Recall')
+    plt.show()
+
+
+filename = 'model.sav'
+try:
+    best_model = pickle.load(open(filename, 'rb'))
+except FileNotFoundError:
+    best_model = None
+
+# Search for hyperparameters if there is no saved model
+if type(best_model) != RandomForestClassifier:
+    # Initialize the Bayesian Search Tuner that maximizes the F2-score
+    tuner = kt.tuners.SklearnTuner(
+        oracle=kt.oracles.BayesianOptimizationOracle(
+            objective=kt.Objective("score", "max"),
+            max_trials=500
+        ),
+        hypermodel=build_model,
+        scoring=metrics.make_scorer(metrics.fbeta_score, beta=2),
+        cv=StratifiedKFold(10),
+        directory='./model/',
+        project_name='random_forest'
     )
 
-# Search for the best hyperparameter values
-tuner.search(X_train, y_train.values.ravel())
-best_hp = tuner.get_best_hyperparameters()[0]
+    # Search for the best hyperparameter values
+    tuner.search(X_train, y_train.values.ravel())
+    best_hp = tuner.get_best_hyperparameters()[0]
 
-# Build and train model
-best_model = tuner.hypermodel.build(best_hp)
-best_model.fit(X_train, y_train.values.ravel())
+    # Build and train model
+    best_model = tuner.hypermodel.build(best_hp)
+    best_model.fit(X_train, y_train.values.ravel())
 
-# Evaluate using test set
-y_pred = best_model.predict(X_test)
-print("PR-AUC:", metrics.average_precision_score(y_test, y_pred))
-tn, fp, fn, tp = metrics.confusion_matrix(y_test, y_pred).ravel()
-print("Specificity:", tn / (tn + fp))
-print("Sensitivity/Recall:", metrics.recall_score(y_test, y_pred))
-print("Precision:", metrics.precision_score(y_test, y_pred))
-print("F2-Score:", metrics.fbeta_score(y_test, y_pred, beta=2))
-print("Confusion Matrix:")
-print("tp:", tp, "fn:", fn)
-print("fp:", fp, "tn:", tn)
+print("Performance on test set:")
+evaluate_model(best_model, X_test, y_test)
+print("\nPerformance on training set:")
+evaluate_model(best_model, X_train, y_train)
 
-#save model
-filename = 'model.sav'
-pickle.dump(best_model, open(filename, 'wb'))
+# Save model if there is no existing model
+if not isfile(filename):
+    pickle.dump(best_model, open(filename, 'wb'))
