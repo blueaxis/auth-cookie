a4a9075c6c7be8e40916ea4af0e05c187fdb9c62
Alarcon Ace Belen <alarconacebelen@gmail.com>
2022-03-18 20:36:16 +0800

    Add method to retrieve cookies from a site

--- app/src/pages/Popup/utils.js
+++ app/src/pages/Popup/utils.js
@@ -1,16 +1,28 @@
 
+const getCurrentTabCookies = async () => {
+  let tab = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+  let cookies = await chrome.cookies.getAll({"url": tab[0].url});
+  return cookies;
+}
+
 // Detect authentication cookies using the random forest model
 const detectCookies = () => {
-  const authCookieNames = []
-  // detection logic goes here
-  alert("Hello Happy World!")
-  return authCookieNames
+  let authCookieNames = [];
+  getCurrentTabCookies().then(
+    (cookies) => {
+      alert("Number of cookies in this site: " + cookies.length)
+      // Send cookies to the Flask API
+    }
+  );
+
+  // Receive cookies from the API
+  return authCookieNames;
 }
 
 // Protect authentication cookies by setting Secure and
 // HTTP-Only flags to true
 export const protectCookies = async () => {
 
-  const authCookieNames = detectCookies()
+  let authCookieNames = detectCookies();
   // protection logic goes here
 }
